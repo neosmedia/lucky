2517d01208e81cbca21fa401e461e1ca7e1f6ded
Ty Lange <neosmedia@gmail.comm>
2024-05-12 09:50:49 -0700

    head link to favicon in images dir

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,10 @@ const Home: NextPage = () => {
           content="Win Degen tokens in the Lucky Jackpot 75/25 draw"
           name="description"
         />
-        <link href="/images/favicon.ico" rel="icon" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       </Head>
 
       <nav>
