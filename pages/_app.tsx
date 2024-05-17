bd76db2dcae733c58285f5949f8efb7f744dd63a
henry <pye.henry@protonmail.com>
2024-05-17 12:49:05 -0400

    Add .env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import {
@@ -12,7 +13,6 @@ import {
   polygon,
   sepolia,
 } from 'wagmi/chains';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
 const config = getDefaultConfig({
   appName: 'Lucky Jackpot',
