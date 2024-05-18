34bc774df03e2d26b440897a3a6018626d3ea782
henry <pye.henry@protonmail.com>
2024-05-18 16:28:43 -0400

    Update config for base mainnet

--- utils/contract-addresses.ts
+++ utils/contract-addresses.ts
@@ -1,9 +1,7 @@
 import { Address } from "viem";
-import { sepolia } from "viem/chains";
+import { base } from "viem/chains";
 import { Chain } from "wagmi/chains";
 
-export const CHAIN: Chain = sepolia;
-export const JACKPOT_ADDRESS = "0x6eD1507076B832E3Bed25DC64d28251A0c409700" as Address;
-export const DEGEN_ADDRESS = "0x362c1e810a81d8116d6673574823767c8caf7f6d" as Address;
-
-//* Neos1 Coinbase Wallet address: 0xb4742d85eb5bae3a86c98a24f5ff6d0aa19f44f4
+export const CHAIN: Chain = base;
+export const JACKPOT_ADDRESS = "0xE956dd84bfeb92b9eaF1324A7dF10E064C171AB8" as Address;
+export const DEGEN_ADDRESS = "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed" as Address;
