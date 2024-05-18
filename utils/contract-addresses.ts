8e253b6d1f8562a4803e7b3a7eff1f94d41df36b
Ty Lange <neosmedia@gmail.comm>
2024-05-17 23:40:15 -0700

    trying to change testnet

--- utils/contract-addresses.ts
+++ utils/contract-addresses.ts
@@ -2,6 +2,6 @@ import { Address } from "viem";
 import { sepolia } from "viem/chains";
 import { Chain } from "wagmi/chains";
 
-export const CHAIN: Chain = sepolia;
+export const CHAIN: Chain = base sepolia;
 export const JACKPOT_ADDRESS = "0x6eD1507076B832E3Bed25DC64d28251A0c409700" as Address;
 export const DEGEN_ADDRESS = "0x362c1e810a81d8116d6673574823767c8caf7f6d" as Address;
