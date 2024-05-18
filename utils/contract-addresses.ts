1a6284f29f8af8069a83c997965912404fc396f8
Ty Lange <neosmedia@gmail.comm>
2024-05-17 23:46:29 -0700

    Back to sepolia and commented my Neos1 address

--- utils/contract-addresses.ts
+++ utils/contract-addresses.ts
@@ -2,6 +2,8 @@ import { Address } from "viem";
 import { sepolia } from "viem/chains";
 import { Chain } from "wagmi/chains";
 
-export const CHAIN: Chain = base sepolia;
+export const CHAIN: Chain = sepolia;
 export const JACKPOT_ADDRESS = "0x6eD1507076B832E3Bed25DC64d28251A0c409700" as Address;
 export const DEGEN_ADDRESS = "0x362c1e810a81d8116d6673574823767c8caf7f6d" as Address;
+
+//* Neos1 Coinbase Wallet address: 0xb4742d85eb5bae3a86c98a24f5ff6d0aa19f44f4
