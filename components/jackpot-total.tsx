ecf9fcc644cbaa052af3e4c55010fcec90e1800c
Ty Lange <neosmedia@gmail.comm>
2024-05-18 16:57:53 -0700

    Jackpot grows until there is a winner

--- components/jackpot-total.tsx
+++ components/jackpot-total.tsx
@@ -34,7 +34,7 @@ export const JackpotTotal = () => {
           {formatEther(jackpotTotal ?? BigInt("0"))} <small>$DEGEN</small>
         </h2>
       )}
-      <p><small>Jackpot grows until there's a winner</small></p>
+      <p><small>Jackpot grows until there is a winner</small></p>
     </>
   );
 };
