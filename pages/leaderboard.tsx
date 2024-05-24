e72d3235d4f731b47c1fe0a3405b133fd76a9315
Ty Lange <neosmedia@gmail.comm>
2024-05-23 17:51:07 -0700

    added 10 rows to leaderboard and leader

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -92,7 +92,31 @@ export default function Leaderboard() {
             <tr>
               <td className={styles.centre}>6</td>
               <td>tester</td>
-              <td className={styles.centre}>0</td>
+              <td className={styles.centre}>--</td>
+              <td className={styles.right}>--</td>
+            </tr>
+            <tr>
+              <td className={styles.centre}>7</td>
+              <td>dev</td>
+              <td className={styles.centre}>--</td>
+              <td className={styles.right}>--</td>
+            </tr>
+            <tr>
+              <td className={styles.centre}>8</td>
+              <td>qa</td>
+              <td className={styles.centre}>--</td>
+              <td className={styles.right}>--</td>
+            </tr>
+            <tr>
+              <td className={styles.centre}>9</td>
+              <td>neos88</td>
+              <td className={styles.centre}>--</td>
+              <td className={styles.right}>--</td>
+            </tr>
+            <tr>
+              <td className={styles.centre}>10</td>
+              <td>web3</td>
+              <td className={styles.centre}>--</td>
               <td className={styles.right}>--</td>
             </tr>
           </tbody>
